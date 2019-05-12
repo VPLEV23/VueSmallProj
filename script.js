dddbcd4197691fcb8eec800910ff7e41b7d2f926
Vlad <bladetheblade12@gmail.com>
2019-05-12 18:22:29 +0200

    Task List

---
+++ script.js
@@ -0,0 +1,27 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        title: "Task list",
+        items: [
+            { text: "First" },
+            { text: "Second" },
+            { text: "First" },
+            { text: "Second" },
+
+        ]
+    },
+    methods: {
+        addItem: function(){
+            let input = document.getElementById('itemForm');
+            if (input.value !== ''){
+                this.items.push({
+                    text: input.value
+                })
+                input.value = "";
+            }
+        },
+        deleteItem: function(index){
+                this.items.splice(index, 1);
+        }
+    }
+})
